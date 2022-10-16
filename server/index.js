36af50a4b1303fb0d920c690f53240123989d70b
Vaibhav Matere <vpm00718897@gmail.com>
2022-10-16 19:18:58 +0530

    setup middleware

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+//middleware
+app.use(cors());
+
+app.listen(5000, () => {
+  console.log("server running on 5000");
+});
