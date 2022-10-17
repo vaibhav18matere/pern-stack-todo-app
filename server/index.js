c99a7729c062604f3f45d60c5a8cddbd1b0944b7
Vaibhav Matere <vpm00718897@gmail.com>
2022-10-17 11:52:18 +0530

    added create todo

--- server/index.js
+++ server/index.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require('./db');
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//create todo
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+
+
+
+
+
+
 
 app.listen(5000, () => {
   console.log("server running on 5000");
