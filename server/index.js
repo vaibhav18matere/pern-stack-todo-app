443fc7486d316cfc878cacb63e43db860d2f8836
Vaibhav Matere <vpm00718897@gmail.com>
2022-10-17 12:56:27 +0530

    feat : delete todo

--- server/index.js
+++ server/index.js
@@ -50,21 +50,30 @@ app.put("/todos/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const { description } = req.body;
-    const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
-    res.json("Todo updated")
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("Todo updated");
   } catch (error) {
     console.log(error.message);
   }
 });
 
+// delete todo
 
-
-
-
-
-
-
-
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query(
+      "DELETE FROM todo WHERE todo_id=$1",
+      [id]
+    );
+    res.json("Todo was deleted");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log("server running on 5000");
