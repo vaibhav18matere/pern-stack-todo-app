4444f76e5f71753d180aafc29a01d4a25083dd02
Vaibhav Matere <vpm00718897@gmail.com>
2022-10-17 11:57:25 +0530

    get all todos

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const pool = require('./db');
+const pool = require("./db");
 
 //middleware
 app.use(cors());
@@ -12,21 +12,26 @@ app.use(express.json());
 app.post("/todos", async (req, res) => {
   try {
     const { description } = req.body;
-    const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *",
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
       [description]
     );
     res.json(newTodo.rows[0]);
   } catch (error) {
     console.log(error);
   }
-})
+});
 
+// get all todo
 
-
-
-
-
-
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log("server running on 5000");
